f159c42856a9a79b25a91f0b656de165b1b9be37
rajmishra-47 <rajnandanmishra2002@gmail.com>
2025-04-15 12:12:04 +0530

    Initial Commit

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Rajnandan Mishra on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App1Tests/App1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  App1Tests.swift
+//  App1Tests
+//
+//  Created by Rajnandan Mishra on 15/04/25.
+//
+
+import Testing
+@testable import App1
+
+struct App1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
